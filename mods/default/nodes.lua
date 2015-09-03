ffb5c49103223270b0971b69c46b5dee08a81e58
kaadmy <kaadmy@gmail.com>
2015-09-02 18:13:14 -0700

    adding support for ubertrees

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -241,6 +241,33 @@ minetest.register_node(
       sounds = default.node_sound_leaves_defaults(),
    })
 
+minetest.register_node( -- loks just like default oak leaves, except they decay much farther
+   "default:leaves_oak_huge",
+   {
+      description = "Oak Leaves(Huge)",
+      drawtype = "allfaces_optional",
+      visual_scale = 1.3,
+      tiles ={"default_leaves_oak.png"},
+      paramtype = "light",
+      waving = 1,
+      is_ground_content = false,
+      groups = {snappy=3, leafdecay=10, fall_damage_add_percent=-5, leaves = 1},
+      drop = {
+	 max_items = 1,
+	 items = {
+	    {
+	       items = {"default:sapling_oak"},
+	       rarity = 40,
+	    },
+	    {
+	       items = {"default:leaves_oak"},
+	       rarity = 10,
+	    }
+	 }
+      },
+      sounds = default.node_sound_leaves_defaults(),
+   })
+
 minetest.register_node(
    "default:sapling_birch",
    {
