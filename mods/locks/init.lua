a3930835aba37747a8cb8095ed728c03713785b4
kaadmy <kaadmy@gmail.com>
2015-10-19 14:06:43 -0700

    reduced owner unlock time for locked chests

--- mods/locks/init.lua
+++ mods/locks/init.lua
@@ -131,7 +131,7 @@ minetest.register_node(
 			 if not locks.is_locked(meta, player) then
 			    if locks.is_owner(meta, player) then
 			       -- also unlock when owner opens for "sharing" locked stuff
-			       meta:set_float("last_lock_pick", minetest.get_gametime() + picked_time)
+			       meta:set_float("last_lock_pick", minetest.get_gametime() + 5)
 			    end
 
 			    local np = pos.x .. "," .. pos.y .. "," .. pos.z
