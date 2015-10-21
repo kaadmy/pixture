048616b0b369e2fa09bc580adfa66d61dedc503a
kaadmy <kaadmy@gmail.com>
2015-10-21 14:51:11 -0700

    removed glass edge, added in commit fd9362bbc1e2bd7b8ce7115ef0619a40145c8ac2

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -681,7 +681,7 @@ minetest.register_node(
    {
       description = "Glass",
       drawtype = "glasslike_framed_optional",
-      tiles ={"default_glass_frame.png", "default_glass.png^default_glass_frame.png"},
+      tiles ={"default_glass_frame.png", "default_glass.png"},
       paramtype = "light",
       sunlight_propagates = true,
       groups = {snappy=2,cracky=3,oddly_breakable_by_hand=2},
