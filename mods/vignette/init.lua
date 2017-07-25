3ac8edbcc584278a54ec502e15b74a640a1c8379
KaadmY <kaadmy@gmail.com>
2017-07-25 09:38:26 -0700

    Add options to disable vignette

--- mods/vignette/init.lua
+++ mods/vignette/init.lua
@@ -3,22 +3,26 @@
 -- By KaadmY, for Pixture
 --
 
-local vignette_definition = {
-   hud_elem_type = "image",
-   position = {x = 0.5, y = 0.5},
-   scale = {x = -100, y = -100},
-   alignment = 0,
-   text = "vignette_vignette.png",
-}
+local enable_vignette = minetest.settings:get_bool("vignette_enable")
 
-local function on_joinplayer(player)
-   if not player:is_player() then
-      return
+if enable_vignette then
+   local vignette_definition = {
+      hud_elem_type = "image",
+      position = {x = 0.5, y = 0.5},
+      scale = {x = -100, y = -100},
+      alignment = 0,
+      text = "vignette_vignette.png",
+   }
+
+   local function on_joinplayer(player)
+      if not player:is_player() then
+         return
+      end
+
+      player:hud_add(vignette_definition)
    end
 
-   player:hud_add(vignette_definition)
+   minetest.register_on_joinplayer(on_joinplayer)
 end
-
-minetest.register_on_joinplayer(on_joinplayer)
 
 default.log("mod:vignette", "loaded")
